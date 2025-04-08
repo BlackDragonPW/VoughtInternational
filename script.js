ecb866b60d86efc5b90daa3b9aa8fb6dc094d618
BlackDragonPW <divyaamshu100@gmail.com>
2025-04-08 23:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function sayHi() {
-  alert("Hi there! Welcome to my site.");
-}
+// Trigger fade-in animations on scroll
+window.addEventListener('scroll', () => {
+  document.querySelectorAll('.fade-in').forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 50) {
+      el.style.animationPlayState = 'running';
+    }
+  });
+});
